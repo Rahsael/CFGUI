db5d7e2e49c1f006456ec499d85c04a5d0621ad5
Rahsael <59663993+Rahsael@users.noreply.github.com>
2021-02-15 23:42:56 -0600

    Created loader.lua
    Allows the loader script to be updated independently of CFGUI.

---
+++ loader.lua
@@ -0,0 +1,17 @@
+function downloadPkg()
+  if exists("CFGUI", "script") > 0 then
+    return
+  else
+    downloadFile(getMudletHomeDir() .. "/CFGUI.zip", "https://github.com/Rahsael/CFGUI/releases/latest/download/CFGUI.zip")  
+  end
+end
+registerAnonymousEventHandler("sysLoadEvent", "downloadPkg")
+
+function installReady(event, file)
+  if file ~= getMudletHomeDir().."/CFGUI.zip" then
+    return
+  else
+    installPackage(getMudletHomeDir().."/CFGUI.zip")
+  end
+end
+registerAnonymousEventHandler("sysDownloadDone", "installReady")
