6fd4a13319454ba61c2df9a53fbf8b5bf44297e4
Rahsael <59663993+Rahsael@users.noreply.github.com>
2022-02-08 21:22:56 -0600

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,10 +1,11 @@
--- Updated 2/16/2021
+-- Updated 2/08/2022
 function downloadPkg()
   if exists("CFGUI", "script") > 0 then
     return
   else
     downloadFile(getMudletHomeDir() .. "/CFGUI.zip", "https://github.com/Rahsael/CFGUI/releases/latest/download/CFGUI.zip")  
   end
+  return
 end
 registerAnonymousEventHandler("sysLoadEvent", "downloadPkg")
 
@@ -12,6 +13,7 @@ function installReady(event, file)
   if file ~= getMudletHomeDir().."/CFGUI.zip" then
     return
   else
+    uninstallPackage("CFGUI")
     installPackage(getMudletHomeDir().."/CFGUI.zip")
   end
 end
@@ -20,7 +22,7 @@ registerAnonymousEventHandler("sysDownloadDone", "installReady")
 function installComplete(_, package)
   if package == "CFGUI" then
     cecho("<OrangeRed>Installation complete!\n\n")
-    cecho("<gold>After logging in, you may need to use the <white>setprompt<gold> command. See <white>guihelp<gold> for more information.\n\n")
+    cecho("<gold>After logging in, you may need to use the <white>score<gold> and <white>setprompt<gold> command.\nSee <white>guihelp<gold> for more information.\n\n")
   end
 end
 registerAnonymousEventHandler("sysInstall", "installComplete")
